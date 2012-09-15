a44bc992e0b88c6f3948522295673efa827c39bd
Kevin L. Mitchell <klmitch@mit.edu>
2012-09-15 00:37:26 -0500

    Begin roughing in Tendril
    Initial commit, with some infrastructure files and the framers with
    unit tests.

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+def readfile(filename):
+    with open(filename) as f:
+        return f.read()
+
+setup(
+    name='Tendril',
+    version='0.1.0',
+    author='Kevin L. Mitchell',
+    author_email='klmitch@mit.edu',
+    url='http://github.com/klmitch/tendril',
+    description='Frame-based Connection Framework',
+    long_description=readfile('README.rst'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        ],
+    packages=['tendril'],
+    requires=[
+        "gevent",
+        "pkg_resources",
+        ],
+    tests_require=[
+        "mock",
+        ],
+    entry_points={
+        'tendril.manager': [
+            ],
+        },
+    )
